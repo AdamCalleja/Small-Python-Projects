59f474362b2d2807d244f23578130ad37f7d7237
Adam Calleja <adam1.calleja@gmail.com>
2022-01-01 13:45:54 +0000

    Fixed minor errors within the code

--- Number-Guessing.py
+++ Number-Guessing.py
@@ -12,7 +12,7 @@ def divisors_of_number(target_number):
             known_divisor = target_number / potential_divisor
             factors_of_target.append(potential_divisor)
             factors_of_target.append(known_divisor)
-    if len(a) == 2: 
+    if len(factors_of_target) == 2: 
         target_prime = True
     else:
         target_prime = False
@@ -27,7 +27,7 @@ def get_hint(factors_of_target, target_prime, hints_used):
 # In the 'easy' game mode the game will automatically give a hint if the guess is incorrect. 
 # In the 'normal' game mode the game will ask the user if they want a hint. 
 # In the 'hard' game mode the user won't get any hints. 
-def guess_target(lower_bound, upper_bound, game_mode): 
+def guess_target(lower_bound, upper_bound, game_mode, target_number): 
     target_found = False
     number_guess = input("Try to guess the number between ", lower_bound, " and ", upper_bound, ": ")
     if int(number_guess) == target_number: 
@@ -36,6 +36,6 @@ def guess_target(lower_bound, upper_bound, game_mode):
         number_comparisson = "low"
     elif number_guess > target_number: 
         number_comparisson = "high"
-    elif game_mode = "easy": 
+    if game_mode == "easy": 
         print("Incorrect")
         print("Your guess was too ", number_comparisson)          
